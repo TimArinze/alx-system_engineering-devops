86390efeb4d8a717f6ac252a00b461f2dcee4710
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-10-18 22:07:04 +0000

    task

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -3,4 +3,4 @@
 # Using the project instructions, create a Ruby script
 # that accepts one argument and
 # pass it to a regular expression matching method
-puts ARGV[0].scan(/hbt{2,5}n/).join
+/hbt{2,5}n/
