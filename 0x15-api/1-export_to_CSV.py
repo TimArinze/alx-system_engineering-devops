87922e330a3d60095f32a56679fbded8b3e1aa0f
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-16 22:10:13 +0100

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,24 +7,25 @@ import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-    main_url = "https://jsonplaceholder.typicode.com"
-    individual = main_url + "/users/{}".format(argv[1])
-    user_todos = main_url + "/user/{}/todos".format(argv[1])
-    employeeDetails = requests.get(individual).json()
-    usersTotalTasks = requests.get(user_todos).json()
+main_url = "https://jsonplaceholder.typicode.com"
 
+Id = int(argv[1])
+if __name__ == "__main__":
+    user_response = requests.get("{}/users/{}".format(main_url, Id)).json()
+    todos = requests.get("{}/user/{}/todos".format(main_url, Id)).json()
+    user_name = user_response.get("username")
     data = []
-    for task in usersTotalTasks:
+    for task in todos:
         dicts = {}
         dicts.update({"userId": argv[1],
-                     "username": employeeDetails.get("username"),
+                     "username": user_response.get("username"),
                       "completed": task.get("completed"),
                       "title": task.get("title")})
         data.append(dicts)
     with open('{}.csv'.format(argv[1]), 'w', encoding='UTF8') as f:
-        header = ["userId", "username", "completed", "title"]
-        writer = csv.DictWriter(f, fieldnames=header, quoting=csv.QUOTE_ALL,
-                                delimiter=",")
-        # write multiple rows
-        writer.writerows(data)
+        for todo in data:
+            f.write('"{}", "{}", "{}", "{}"\n'.format(
+                Id,
+                user_name,
+                todo.get('completed'),
+                todo.get('title')))
