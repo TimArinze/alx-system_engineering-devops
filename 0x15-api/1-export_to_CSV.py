008efa3aaafc7397de4ad45039a1d66abe37b824
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-16 23:13:59 +0100

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,25 +7,20 @@ import requests
 from sys import argv
 
 
-main_url = "https://jsonplaceholder.typicode.com"
-
-Id = int(argv[1])
 if __name__ == "__main__":
-    user_response = requests.get("{}/users/{}".format(main_url, Id)).json()
-    todos = requests.get("{}/user/{}/todos".format(main_url, Id)).json()
-    user_name = user_response.get("username")
+    main_url = "https://jsonplaceholder.typicode.com"
+    user_res = requests.get("{}/users/{}".format(main_url, argv[1])).json()
+    todos = requests.get("{}/user/{}/todos".format(main_url, argv[1])).json()
+    user_name = user_res.get("username")
     data = []
     for task in todos:
         dicts = {}
         dicts.update({"userId": argv[1],
-                     "username": user_response.get("username"),
+                     "username": user_res.get("username"),
                       "completed": task.get("completed"),
                       "title": task.get("title")})
         data.append(dicts)
     with open('{}.csv'.format(argv[1]), 'w', encoding='UTF8') as f:
-        for todo in data:
-            f.write('"{}", "{}", "{}", "{}"\n'.format(
-                Id,
-                user_name,
-                todo.get('completed'),
-                todo.get('title')))
+        header = ['userId', 'username', 'completed', 'title']
+        writer = csv.DictWriter(f, fieldnames=header, quoting=csv.QUOTE_ALL)
+        writer.writerows(data)
