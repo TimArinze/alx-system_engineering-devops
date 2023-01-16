04d044075a356b153b78d731a1a023850ee1ffbf
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-16 23:30:21 +0100

    task

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    main_url = "https://jsonplaceholder.typicode.com"
+    user_res = requests.get("{}/users/{}".format(main_url, argv[1])).json()
+    todos = requests.get("{}/user/{}/todos".format(main_url, argv[1])).json()
+    user_name = user_res.get("username")
+    data = []
+    for task in todos:
+        dicts = {}
+        dicts.update({"task": task.get("title"),
+                      "completed": task.get("completed"),
+                      "username": user_res.get("username")})
+        data.append(dicts)
+    dictionary = {}
+    dictionary.update({argv[1]: data})
+    with open('{}.json'.format(argv[1]), 'w', encoding='UTF8') as f:
+        json.dump(dictionary, f)
