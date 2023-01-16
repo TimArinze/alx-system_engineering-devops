c175f14286efeda978b73ab5cc74aa15f91cd576
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-16 21:43:01 +0100

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,21 +9,21 @@ from sys import argv
 
 if __name__ == "__main__":
     main_url = "https://jsonplaceholder.typicode.com"
-    users_url = main_url + "/users"
     individual = main_url + "/users/{}".format(argv[1])
     user_todos = main_url + "/user/{}/todos".format(argv[1])
     employeeDetails = requests.get(individual).json()
     usersTotalTasks = requests.get(user_todos).json()
+
     data = []
-    header = ["userId", "username", "completed", "title"]
     for task in usersTotalTasks:
         dicts = {}
         dicts.update({"userId": argv[1],
-                     "username": employeeDetails['username'],
+                     "username": employeeDetails.get("username"),
                       "completed": task.get("completed"),
                       "title": task.get("title")})
         data.append(dicts)
     with open('{}.csv'.format(argv[1]), 'w', encoding='UTF8') as f:
+        header = ["userId", "username", "completed", "title"]
         writer = csv.DictWriter(f, fieldnames=header, quoting=csv.QUOTE_ALL,
                                 delimiter=",")
         # write multiple rows
