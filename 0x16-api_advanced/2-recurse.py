a454523eb01c6e466d7d3d5ae49f158c94610fe7
Arinze Nwutobo <tim.arinze@outlook.com>
2023-02-06 23:16:41 +0100

    task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,22 +6,21 @@ import json
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
+def recurse(subreddit, hot_list=[], after=""):
     """recursive function"""
     url = "https://api.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'user-agent': 'MY_AGENT'}
-    params = {'after': after, 'count': count, 'limit': 100}
+    params = {'after': after, 'limit': 100}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 200:
         results = response.json().get("data")
         after = results.get("after")
-        count += results.get("dist")
         for value in results.get("children"):
             hot_list.append(value.get("data").get("title"))
 
         if after is not None:
-            return recurse(subreddit, hot_list, after, count)
+            return recurse(subreddit, hot_list, after)
         return hot_list
     else:
         return None
