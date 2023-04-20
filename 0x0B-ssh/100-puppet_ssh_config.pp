5b0eb5b73f5696dcd684025956ab4d22a7027b5e
Arinze Nwutobo <tim.arinze@outlook.com>
2023-04-20 03:22:24 +0100

    advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#Your SSH client configuration must be configured to use the
+# private key ~/.ssh/school
+#Your SSH client configuration must be configured
+#to refuse to authenticate using a password
+$public_key = ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKAYwA7g+vnJ5XBV8lamCQo5jTaSJaJS6sMFYcaGhNWucItZjcKMdGj7lkleEFV2x00b9643oac0SeOXljTa3V4In7JlHV0Toot7SI7dZjE9Dgiq/+xRJmBZXs1MCf8QK5XS8rS+2i9JlXD52OZuX6iAqPo7ELASSf6PGyZotj+EdXSvq8bYfUoPW9HYDgfaxCpSgtnQ7tdTuZWjdCE+sBLpC+2Rl7s1LfNQWTQ5uPtXyxwUhrEWrwIClGMp5Wfl0N7DuX+3ZP6igaCDMtjgRCnqtvB27h0gQPHlV2zycf0bjGlHL7USSg/hNMm0EKdkPjcDEQAAkLRSF4OK+uQfU3 alx_applicant_server
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4yljFImN7aPWgBSeXvTyF0Q9uDVYCiY+//BXPdG6E3+cv01lIGaYuWOAHBwodByXxuS2jIbr1kE2Lbe62hhGE2VchhSuxyExZ2bXUZ/mj35AebuIaeVueV9pwQnzDLpKxcVF2QrFaAV75kBVytAaZn4nMV91Na8J35W/yXnOrtj/4g9sMeO2lWkz1acTfri+Zz9z4aFsEV3ulqmbuDkHkZt0S9BjKsQ2ezdnKvAzULpPUun5OjayDhCxuNhjexsAXe0bmWltpu05SWdazmTlwBdR6a0r8I2vSnlSINUOLaykuhtSWJfk34hVmGW4uCHmgB9g9YQNw0JKM36v1aG/KtG95SAtO8kxVbDmcbzQHLVuzr6xkV8w87XGJPL6WeFhHa3BbpepY24OEUR6oMOczeyDt0E/y/jEKHLGowfiUhyb15bxOHKj4o6xEMXAwd8T72ovBpr2nunrZWQkPGAvqbz5+qQXo3LM9op4iNtbdIFvc+2wsML97VeJsApM54xE= vagrant@ubuntu-focal
+
+class ssh_config {
+	ssh_authorized_key {'ubuntu@54.237.110.211':
+		ensure => present,
+		user => '60552-web-01',
+		type => 'ssh-rsa',
+		path => '~/.ssh/school',
+		key => $public_key,
+}
+} 
