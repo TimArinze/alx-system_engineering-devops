8abc92a0c28f0a4985fbe93b178814488ec3c8b5
Arinze Nwutobo <tim.arinze@outlook.com>
2023-04-25 02:23:18 +0100

    task

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""
-Gather data from an API
-"""
-import json
-import requests
+"""fetches information from JSONplaceholder API and exports to JSON"""
+
+from json import dump
+from requests import get
 from sys import argv
 
 
 if __name__ == "__main__":
-    main_url = "https://jsonplaceholder.typicode.com"
-    user_res = requests.get("{}/users/{}".format(main_url, argv[1])).json()
-    todos = requests.get("{}/user/{}/todos".format(main_url, argv[1])).json()
-    user_name = user_res.get("username")
-    data = []
-    for task in todos:
-        dicts = {}
-        dicts.update({"task": task.get("title"),
-                      "completed": task.get("completed"),
-                      "username": user_res.get("username")})
-        data.append(dicts)
-    with open('{}.json'.format(argv[1]), 'w', encoding='UTF8') as f:
-        json.dump({argv[1]: data}, f)
+    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
+        argv[1])
+    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
+
+    todo_list = []
+    for todo in todo_result:
+        todo_dict = {}
+        todo_dict.update({"task": todo.get("title"), "completed": todo.get(
+            "completed"), "username": name_result.get("username")})
+        todo_list.append(todo_dict)
+
+    with open("{}.json".format(argv[1]), 'w') as f:
+        dump({argv[1]: todo_list}, f)
