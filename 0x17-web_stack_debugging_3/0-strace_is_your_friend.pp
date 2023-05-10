ffdda6ef50da32b093401024e4020e91534beeed
Arinze Nwutobo <tim.arinze@outlook.com>
2023-05-11 00:23:17 +0100

    task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Strace is your friend
+# Debugging Apache server returning 500 error
+
+exec {'fix-wordpress':
+	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+	path => '/usr/local/bin/:/bin/'
+}
