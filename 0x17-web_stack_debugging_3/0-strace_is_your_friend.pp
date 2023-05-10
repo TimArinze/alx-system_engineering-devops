b2c28f133e62cd7a54febf992a40a0ce7aad2ad5
Arinze Nwutobo <tim.arinze@outlook.com>
2023-05-11 00:34:56 +0100

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
- fix apache returning 500 error
+# fix apache returning 500 error
 
 exec { 'fix-wordpress':
         command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
         path => '/usr/local/bin/:/bin/'
 }
-
