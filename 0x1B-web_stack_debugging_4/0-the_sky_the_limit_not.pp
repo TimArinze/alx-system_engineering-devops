04a985188c978cca5ac0fcc88800fce5c7280982
Arinze Nwutobo <tim.arinze@outlook.com>
2023-05-18 15:04:15 +0100

    task

--- 0x1B-web_stack_debugging_4/0-the_sky_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_the_limit_not.pp
@@ -2,13 +2,13 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i s/15/4096/g /etc/default/nginx',
+  command  => 'sed -i s/15/4096/g /etc/default/nginx',
   provider => shell
 }
 
 #restart Nginx
 
 exec { 'nginx-restart':
-  command => 'sudo service nginx restart',
+  command  => 'sudo service nginx restart',
   provider => shell
 }
