214540d878cfa8873af830774f76236306076d78
Arinze Nwutobo <tim.arinze@outlook.com>
2023-05-18 14:37:43 +0100

    task

--- 0x1B-web_stack_debugging_4/0-the_sky_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_the_limit_not.pp
@@ -1,11 +1,12 @@
 # fix nginx server of slow performance
 
 exec { 'fixing nginx':
-	command => 'sed -i s/"/usr/share/nginx/html"/"/var/www/html"/g /etc/nginx/sites-available/default,
-	path => '/usr/local/bin/:/bin/'
+  command => 'sed -i s//usr/share/nginx/html//var/www/html/g /etc/nginx/sites-available/default',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 # restarting nginx
 exec { 'nginx restart':
-	command => "sudo service nginx restart"
-	path => "/etc/init.d/"
+  command => 'sudo service nginx restart',
+  path    => '/etc/init.d/'
+}
